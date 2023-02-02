f083aeafab684a73dc260c009777aac4a9b4b3ab
GenerEcheverria <generalex@live.com.mx>
2023-02-02 01:43:40 -0600

    update

--- pokescript.sh
+++ pokescript.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-curl -s https://pokeapi.co/api/v2/pokemon/$1 | 
-jq -jr '"id="+(.id|tostring), " name="+.name, " weight="+(.weight|tostring), " height="+(.height|tostring) , " order="+(.order|tostring) +"\n"' 
+if [[ -z $1 ]]
+  then
+    echo "Debes ingresar un pokemon como parámetro"
+  else 
+    status=$(curl --write-out '%{http_code}' --silent --output /dev/null https://pokeapi.co/api/v2/pokemon/$1)
+    if [[ "$status" -ne 200 ]] ; 
+      then
+        echo "Pokemon no encontrado"
+      else
+        curl -s https://pokeapi.co/api/v2/pokemon/$1 | 
+        jq -jr '"id="+(.id|tostring), " name="+.name, " weight="+(.weight|tostring), " height="+(.height|tostring) , " order="+(.order|tostring) +"\n"' 
+    fi
+fi
